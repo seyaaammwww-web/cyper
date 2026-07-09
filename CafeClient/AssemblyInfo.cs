967a16cebf6698b0725be09b64c3020d172ebf73
seyaaammwww-web <seyaaammwww@gmail.com>
2026-07-09 17:02:40 +0300

    Add/Update CafeClient/AssemblyInfo.cs

---
+++ CafeClient/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Cafe Client")]
+[assembly: AssemblyDescription("Cyber Cafe Client Application for PC monitoring and snack ordering")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Cyber Cafe Management")]
+[assembly: AssemblyProduct("Cafe Client")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a1b2c3d4-e5f6-7890-abcd-ef1234567890")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
